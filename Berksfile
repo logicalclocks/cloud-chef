a6e108b69bc30cdf6e91b7be6712e889852380b4
Antonios Kouzoupis <kouzoupis.ant@gmail.com>
2019-12-03 17:42:46 +0100

    [prepare_env] Prepare cloud anaconda env
    [prepare_env] change name
    
    [prepare_env] Fixes
    
    [prepare_env] Put ec2init in a separate repository
    
    [prepare_env] Download and install ec2init program
    
    [prepare_env] Fixes
    
    [prepare_env] Fixes
    
    [prepare_env] Configuration for ec2init and script to configure Glassfish with lets encrypt certificate
    
    [prepare_env] Update conda env
    
    [prepare_env] Template ec2init configuration file

--- Berksfile
+++ Berksfile
@@ -1,3 +1,7 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+cookbook 'conda', github: "logicalclocks/conda-chef", branch: "master"
+cookbook 'kagent', github: "logicalclocks/kagent-chef", branch: "master"
+cookbook 'hopsworks', github: "logicalclocks/hopsworks-chef", branch: "master"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,36 @@
-#
-# Cookbook:: cloud-chef
-# Recipe:: default
-#
-# Copyright:: 2019, The Authors, All Rights Reserved.
+bash "remove cloud-environment" do
+    user "root"
+    group "root"
+    cwd "/home/#{node['conda']['user']}"
+    code <<-EOF
+        #{node['conda']['base_dir']}/bin/conda env remove -y -q -n cloud
+    EOF
+    only_if "test -d #{node['conda']['base_dir']}/envs/cloud", :user => node['conda']['user']
+end
+
+template "/home/#{node['conda']['user']}/cloud-environment.yml" do
+    source "cloud-environment.yml.erb"
+    user node['conda']['user']
+    group node['conda']['group']
+    mode 0550
+end
+
+bash "create cloud-environment" do
+    user node['conda']['user']
+    group node['conda']['group']
+    cwd "/home/#{node['conda']['user']}"
+    environment ({ 'HOME' => ::Dir.home(node['conda']['user']), 'USER' => node['conda']['user'] })    
+    code <<-EOF
+        #{node['conda']['base_dir']}/bin/conda env create -q --file cloud-environment.yml
+    EOF
+    not_if "test -d #{node['conda']['base_dir']}/envs/cloud", :user => node['conda']['user']
+end
+    
+bash "install ec2init" do
+    user node['conda']['user']
+    group node['conda']['group']
+    code <<-EOF
+      yes | #{node['conda']['base_dir']}/envs/cloud/bin/pip install --no-cache-dir --upgrade #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl
+    EOF
+    only_if "test -r #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl", :user => node['conda']['user']
+end
