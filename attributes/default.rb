039daaf97d991ad4cb4254d37fc31b04ed98eced
Antonios Kouzoupis <kouzoupis.ant@gmail.com>
2020-01-10 12:21:27 +0100

    [1.1] Bump ec2-init version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['cloud']['init']['install_dir']                = '/root'
-default['cloud']['init']['version']                    = '0.1'
+default['cloud']['init']['version']                    = '0.2'
 
 default['cloud']['init']['config']['hosted_zone']      = "aws.hopsworks.ai"
 default['cloud']['init']['config']['lets_encrypt_dir'] = "/etc/letsencrypt"
