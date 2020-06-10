5e0a950c31cad644371235256d512e3d6a0b3744
Antonios Kouzoupis <antonios@logicalclocks.com>
2020-06-10 09:48:30 +0200

    [mute_reports] Add development flag

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['cloud']['init']['version']                    = '0.6'
 
 default['cloud']['init']['config']['hosted_zone']      = "aws.hopsworks.ai"
 default['cloud']['init']['config']['lets_encrypt_dir'] = "/etc/letsencrypt"
+default['cloud']['init']['config']['dev']              = "false"
