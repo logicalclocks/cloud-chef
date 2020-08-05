1e690a7f7a2caab44cfa6d6226cac7dc1da0af93
Antonios Kouzoupis <antonios@logicalclocks.com>
2020-08-05 12:15:32 +0200

    [cloud_domain_name] Change hosted zone
    [cloud_domain_name] Change instance domain name regex

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['cloud']['init']['install_dir']                    = '/root'
 default['cloud']['init']['version']                        = '0.6'
 
-default['cloud']['init']['config']['hosted_zone']          = "aws.hopsworks.ai"
+default['cloud']['init']['config']['hosted_zone']          = "cloud.hopsworks.ai"
 default['cloud']['init']['config']['lets_encrypt_dir']     = "/etc/letsencrypt"
 default['cloud']['init']['config']['dev']                  = "false"
 
