95b0ba0787a0cf9bbe5b2141b625d7505f64e17e
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-10-06 11:05:28 +0200

    [gpu] Gpu support (#27) (#28)
    * [gpu] Template nvidia download url to ec2-init configuration
    
    * [gpu] Get nvidia packages download url from configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+include_attribute 'tensorflow'
+include_attribute 'hops'
+
 default['cloud']['init']['install_dir']                    = '/root'
 default['cloud']['init']['version']                        = '0.6'
 
@@ -10,3 +13,6 @@ default['cloud']['init']['awscli']['url']                  = "#{node['download_u
 default['cloud']['init']['docker']['config_dir']           = "/root/.docker"
 default['cloud']['init']['docker']['ecr-login']['version'] = "0.4.0"
 default['cloud']['init']['docker']['ecr-login']['url']     = "#{node['download_url']}/cloud/ecr-login/#{node['cloud']['init']['docker']['ecr-login']['version']}/docker-credential-ecr-login"
+
+default['cloud']['init']['gpu']['driver_url']              = node['nvidia']['driver_url']
+default['cloud']['init']['gpu']['pkgs_url']                = node['hops']['nvidia_pkgs']['download_url']
