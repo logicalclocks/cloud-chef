ff540a96e0fb730165615276e9c4436f57615c3c
Antonios Kouzoupis <antonios@logicalclocks.com>
2021-08-20 15:48:46 +0300

    [ndb-agent_version] Add version at ndb-agent filename

--- recipes/ndb.rb
+++ recipes/ndb.rb
@@ -25,7 +25,8 @@ directory "#{node['cloud']['ndb-agent']['config']}" do
     mode "700"
 end
 
-filename = "ndb-agent.tgz"
+ndb_agent_folder = "ndb-agent-#{node['cloud']['ndb-agent']['version']}"
+filename = "#{ndb_agent_folder}.tgz"
 source = "#{node['install']['enterprise']['download_url']}/ndb-agent/#{node['cloud']['ndb-agent']['version']}/#{filename}"
 remote_file "#{Chef::Config['file_cache_path']}/#{filename}" do
     user 'root'
@@ -43,10 +44,10 @@ bash "Setup binaries" do
     code <<-EOH
         set -e
         tar xzf #{Chef::Config['file_cache_path']}/#{filename} -C #{Chef::Config['file_cache_path']}
-        cp #{Chef::Config['file_cache_path']}/ndb-agent/ndb-agent #{node['cloud']['ndb-agent']['bin']}
+        cp #{Chef::Config['file_cache_path']}/#{ndb_agent_folder}/ndb-agent #{node['cloud']['ndb-agent']['bin']}
         chmod 750 #{node['cloud']['ndb-agent']['bin']}/ndb-agent
 
-        cp -r #{Chef::Config['file_cache_path']}/ndb-agent/contrib #{node['cloud']['ndb-agent']['bin']}
+        cp -r #{Chef::Config['file_cache_path']}/#{ndb_agent_folder}/contrib #{node['cloud']['ndb-agent']['bin']}
         chmod -R 750 #{node['cloud']['ndb-agent']['bin']}/contrib
     EOH
     not_if { File.exist? "#{node['cloud']['ndb-agent']['bin']}/ndb-agent" }
