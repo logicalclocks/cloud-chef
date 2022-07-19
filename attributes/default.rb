24999bbc582191a5df73d8f1db5d54423cfb2bda
Antonios Kouzoupis <antonios@logicalclocks.com>
2022-07-19 10:11:17 +0200

    [bump_ec2-init] Bump ec2-init version

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,7 +16,7 @@ default['cloud']['data_volume']['mount-point']             = "/hopsworks_data"
 default['cloud']['data_volume']['ec2init_checks']          = "#{node['cloud']['data_volume']['root_dir']}/ec2init_checks"
 
 default['cloud']['init']['install_dir']                    = '/root'
-default['cloud']['init']['version']                        = '0.13'
+default['cloud']['init']['version']                        = '0.14'
 
 default['cloud']['init']['config']['hosted_zone']          = "cloud.hopsworks.ai"
 default['cloud']['init']['config']['lets_encrypt_dir']     = "/etc/letsencrypt"
