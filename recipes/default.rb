8d83285dbb4bd190fc134e0de86b8916edb76516
Antonis Kouzoupis <antonios@logicalclocks.com>
2024-03-27 09:42:14 +0100

    [CLOUD-721] Switch to virtualenv from conda (#196)
    * [CLOUD-721] Switch to virtualenv from conda
    
    * [CLOUD-721] Fix download url
    
    * [CLOUD-721] Fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,38 +1,22 @@
-bash "remove cloud-environment" do
-    user "root"
-    group "root"
-    cwd "/home/#{node['conda']['user']}"
+bash "create virtualenv" do
+    user 'root'
+    group 'root'
+    cwd '/root'
     code <<-EOF
-        #{node['conda']['base_dir']}/bin/conda env remove -y -q -n cloud
+      set -e
+      virtualenv #{node['cloud']['init']['venv']}
+      source #{node['cloud']['init']['venv']}/bin/activate
+      pip install -r #{Chef::Config['file_cache_path']}/ec2init-requirements.txt
     EOF
-    only_if "test -d #{node['conda']['base_dir']}/envs/cloud", :user => node['conda']['user']
-end
-
-template "/home/#{node['conda']['user']}/cloud-environment.yml" do
-    source "cloud-environment.yml.erb"
-    user node['conda']['user']
-    group node['conda']['group']
-    mode 0550
-end
-
-bash "create cloud-environment" do
-    user node['conda']['user']
-    group node['conda']['group']
-    cwd "/home/#{node['conda']['user']}"
-    environment ({ 'HOME' => ::Dir.home(node['conda']['user']), 'USER' => node['conda']['user'] })    
-    code <<-EOF
-        #{node['conda']['base_dir']}/bin/conda env create -q --file cloud-environment.yml
-    EOF
-    not_if "test -d #{node['conda']['base_dir']}/envs/cloud", :user => node['conda']['user']
 end
     
 bash "install ec2init" do
-    user node['conda']['user']
-    group node['conda']['group']
+    user 'root'
+    group 'root'
     code <<-EOF
-      yes | #{node['conda']['base_dir']}/envs/cloud/bin/pip install --no-cache-dir --upgrade #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl
+      yes | /root/#{node['cloud']['init']['venv']}/bin/pip install --no-cache-dir --upgrade #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl
     EOF
-    only_if "test -r #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl", :user => node['conda']['user']
+    only_if "test -r #{Chef::Config['file_cache_path']}/ec2init-#{node['cloud']['init']['version']}-py3-none-any.whl"
 end
 
 template "#{node['cloud']['init']['install_dir']}/ec2init/init_hops-ca.sh" do
